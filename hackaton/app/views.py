007583a92f9bd0ece31dfaf7e1e3e9e8d1e455b2
Anmapu <antilao987@gmail.com>
2019-07-07 03:13:08 -0400

    consumiendo api e identificando elementos claves

--- hackaton/app/views.py
+++ hackaton/app/views.py
@@ -26,7 +26,9 @@ class Analize(View):
 		#return HttpResponse(json.dumps(response, indent=2))
 
 	def get_data(request):
-		url = 'https://api.twitter.com/1.1/search/tweets.json?q=earthquakeLA&count=100'
+		#url = 'https://api.twitter.com/1.1/search/tweets.json?q=earthquakeLA&count=100'
+		disasters = ['earthquake', 'volcano', 'hurricane', 'wildfire', 'storm', 'flood', 'tsunami', 'seaquake', 'asap', 'epstein', 'marreta', '#七夕の願い事']
+		url = 'https://api.twitter.com/1.1/trends/place.json?id=1'
 
 		# Using OAuth1Session
 		oauth = OAuth1('g63VYD1tcsKrRDK681Zm9oYxe',
@@ -36,9 +38,80 @@ class Analize(View):
 
 
 		# Using OAuth1 auth helper
+		#obtener trends globales
 		r = requests.get(url=url, auth=oauth)
+		trends = r.json()
+		for disaster in disasters:
+			for trend in trends[0]['trends']:
+				search = trend['url']
+				search_params = search.split('?')
+				params = search_params[1].lower()
+				if disaster in params:
+					print("holi")
+					print(params)
+					search_url = 'https://api.twitter.com/1.1/search/tweets.json?'+params
+					r = requests.get(url=search_url, auth=oauth)
+		search = trends[0]['trends'][0]['url']
+		#obtener parametros de busqueda de tweets
+		search_params = search.split('?')
+		search_url = 'https://api.twitter.com/1.1/search/tweets.json?'+search_params[1]
+		r = requests.get(url=search_url, auth=oauth)
 		twits = r.json()
+		texto = ''
+		for twit in twits['statuses']:
+			texto = texto + twit['text']
 		data = json.dumps(twits, indent=2)
+
+		natural_language_understanding = NaturalLanguageUnderstandingV1(
+    		version='2018-11-16',
+    		iam_apikey='VUJWvResSognuEFqEu3GAi_mBcR1fmFvrce-5JRWLhqr',
+    		url='https://gateway.watsonplatform.net/natural-language-understanding/api'
+			)
+
+		response = natural_language_understanding.analyze(
+    	text=texto,
+    	features=Features(concepts=ConceptsOptions(limit=20))).get_result()
+		data = json.dumps(response, indent=2)
+		print(json.dumps(response, indent=2))
+		return JsonResponse(json.loads(data), safe=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 		return JsonResponse(json.loads(data), safe=False)
     	
